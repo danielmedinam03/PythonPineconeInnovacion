ea8c07aa9e2c05441dc5d5ef5ef6b348d9eb811f
Henry Valdivia <hvaldivia@soaint.com>
2024-04-07 00:38:30 -0500

    Se modulariza la applicacion y se agregan mejoran agregando nuevos elementos

---
+++ Wiki.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from PIL import Image
+
+# Streamlit de presentación, con titulo y resumen de proyecto
+
+st.set_page_config(
+    page_title="Aplicacion de RRHH",
+    page_icon=":chart_with_upwards_trend:"
+)
+
+st.title("Análisis de Perfiles")
+
+st.write("""
+Este proyecto MVP tiene como objetivo obtener a los candidatos ideales de SOAINT con respecto a un requerimiento 
+por parte del equipo de de Preventa y/o Operaciones.\n
+Esto permitirá al  equipo de RRHH identificar aquellos perfiles requeridos de forma inmediata 
+para dar soporte tanto a las ventas como la operativa del negocio.\n
+Se realizo previamente la carga de la data de las hojas de vida de los candidatos que sirven como data para el análisis. \n
+A continuación, se explica brevemente cada uno de los módulos del sistema
+""")
+
+image = Image.open("images/rrhh.jpg")
+st.image(image, use_column_width=True)
+
+st.header("Módulos del Sistema")
+
+st.write("""
+- **Wiki:** En esta sección se explica todo lo referente a la aplicación.
+
+- **Análisis de Perfil:** En esta sección ingrese su documento de requerimiento y luego solicite los candidatos que se ajusten a dichos requerimientos.
+
+- **Carga de Archivos:** En esta sección podrá cargar hojas de vida nuevas para el procesamiento de perfiles.
+""")
---
+++ pages/Carga_Archivos.py
@@ -0,0 +1,270 @@
+import streamlit as st
+import fitz  # PyMuPDF
+from dotenv import load_dotenv
+import openai
+import os
+import docx  # Importa el módulo completo en lugar de solo 'Document'
+import app_pinecone
+
+informacion_recopilar = '''
+Datos Personales
+Identificación:
+Datos que permiten identificar a un postulante.
+Fecha Modificación: Fecha en que se modifica el registo.
+Pais: País donde vive el postulante.
+Nombres: Uno o mas nombres de pila del postulante, puede incluir Abreviaciones.
+Apellidos: Principalmente el Apellido Paterno, puede incluir el apellido Materno
+Tipo Identificación: Identificación del estado, depende del país emisor, puede ser [
+    "rut",
+    "dni",
+    "cedula",
+    "Pasaporte",
+    "Número de seguridad Social",
+    "Número Licencia de Conducir"
+]
+Identificación: Número de identificación, serial de identificación, puede contener números, letras y dígitos de verificación.
+Edad: Edad del Postulante al momento de guardar el registro, puede ser calculada a partir de la fecha de nacimiento.
+
+Contacto
+Datos que permiten contactar al postulante.
+Fecha Modificación: Fecha en que se modifica el registo.
+Ciudad: Ciudad donde vive el postulante.
+Comuna: Comuna donde vive el postulante
+Dirección: Dirección donde vive el postulante
+Mail: Mail personal del postulante, puede ser un mail corporativo.
+Telefono: Puede ser el Celular o teléfono fijo.
+
+Redes
+Datos relativos a redes sociales, comunidades en líneas, blogs en línea, Páginas personales, Portafolios personales.
+Fecha Modificación: : Fecha en que se modifica el registo.
+tipo red: Puede incluir [
+    "linkedin",
+    "Facebook",
+    "gitlab",
+    "docker"
+]
+enlace link usuario: Enlace o link a la red social o nombre de usuario.
+
+Perfil Profesional
+Datos referentes a los estudios realizados por el postulante o a su experiencia empírica.
+Perfil de estudios: Estudios realizados por el postulante.
+Fecha Modificación: Fecha en que se modifica el registo.
+ Nivel más alto: Nivel de estudio mas alto alcanzado por el postulante en orden de importancia son [
+    "Post Doctorado",
+    "Doctorado",
+    "Maestría",
+    "Carrera Universitaria Completa",
+    "Carrera Universitaria Inconpleta",
+    "Carrera Técnica Completa",
+    "Carrera Técnica Inconpleta",
+    "Autodidacta",
+    "Estudiante"
+]
+ Título: Título obtenido, nombre del curso, Nombre de la carrera, Nombre de la especialización.
+ Año de finalización: Año en que termina el curso.
+ Años de experiencia: Años en que ha tenido experiencia en campo después de obter el Título a partir del año de finalización
+
+Auto percepción profesional
+Como se presenta o persibe el postulante de acuerdo a su propia descripción. Puede ser el cargo al que postula, Cargo al que es recomendado, cargo más alto alcanzado según su experiencia profesional.
+Fecha Modificación: Fecha en que se modifica el registo.
+
+Objetivos: Texto escrito por el aspirante donde declara sus objetivos, metas a largo y corto plazo, como se visualiza en la empresa, que aportará a la empresa, que desea hacer en el futuro.
+Habilidades: rasgos interpersonales indicados por el postulante que ha desarrollado con el tiempo basado en su experiencia profesional, pueden ser comunicación acertiva, liderazgo, comunicación y escucha activa, planificación y Gestión del tiempo, flexibilidad, negociación.
+Intereses: Intereses profesionales del postulante.
+
+Conocimientos
+Conocimientos específicos del postulante para su desarrollo profesional.
+Fecha Modificación Fecha en que se modifica el registo.
+Tecnologías": """Herramientas": """Certificado": """Años de experiencia": """Clasificación de experiencia": ""
+
+Perfil Psicológico
+Datos de la conducta del postulante o que permitan determinar su empleabilidad segura en la sociedad.
+Fecha Modificación: Fecha en que se modifica el registo.
+Deportes: Deportes que practica, o clubes deportivos a los que pertenece.
+Aficiones pasatiempos: Aficciones pasatiempos hobbies.
+Familia: Actividades que realiza el postulante en familia, datos sobre su familia o su iteración con esta, como se describe el postulante con respecto a su familia, lo que comenta el postulante sobre la familia.
+Sociedad: Actividades que realiza el postulante en la sociedad diferente a deportes, como conciertos, comidas, actividades al aire libre,  actividades religiosas, actividads altruistas, clubes sociales.
+
+Competencias y Habilidades
+Competencias y habilidades blandas.
+Fecha Modificación: Fecha en que se modifica el registo.
+Habilidades blandas: habilidades descritas en las funciones o experiencia profesional pueden ser [
+    "Adaptabilidad",
+    "Pensamiento Crítico",
+    "Pensamiento Creativo",
+    "Trabajo en Equipo",
+    "Resolución de Problemas",
+    "Etica Laboral",
+    "Liderazgo",
+    "Gestión del tiempo"
+]
+
+Idiomas
+Conocimientos de idiomas del postulante
+Fecha Modificación: Fecha en que se modifica el registo.
+Nivel: medición del grado de compresión del idioma del postulante puede ser  [
+    "Nativo",
+    "Avanzado",
+    "Medio",
+    "Básico"
+]
+
+Experiencia
+Experiencia profesional del postulante
+Fecha Modificación: Fecha en que se modifica el registo.
+Administración: Tipo de empresa según su administración puede ser [
+    "Privado",
+    "Público",
+    "Mixto"
+]
+Sector: Clasificación de la empresa según su actividad económica Sector primario: Empresas dedicadas a extraer y aprovechas materias primas Ejemplo agricultura, ganadería, pesca, minería, caza, recursos forestales.
+Sector secundario: Empresas dedicadas a la manipulación de los recursos naturales, transformación bienes en tros más útiles Ejemplo industria, contrucción, fábricas
+Sector terciario: Empresas dedicadas a prestar servicios Ejemplo: Telefonía, comercio, transporte y comunicaciones
+Sector cuaternario: Empresas dedicadas a investigación y tecnología
+Sector quinario: Empresas dedicadas a  educación salud y cultura
+Nombre Empresa:Nombre de la empresa en que laboró el postulante
+Cargo: último cargo que desempeñó el postulante.
+Principales Funciones: Principales funcionaes realizadas por el postulante
+Meses ejerciendo: Cantidad de meses que postulante trabajó en la emrpesa
+Cantidad de personas a cargo: cantidad de personas bajo el mando o administración del postulante.
+
+
+
+Proyectos
+Proyectos en los que ha participado el postulante
+Fecha Modificación Fecha en que se modifica el registo.
+Tipo de proyecto: Tipo o Nombre del proyecto en el que participó.
+ Jefe de proyecto: Nombre del jefe del proyecto o encargado del proyecto o jefe inmediato.
+Facturable: Si el proyecto fué facturable o no [
+    "Si",
+    "No"
+]
+Cargo en el proyecto: Cargo del postulante en el proyecto puede ser [
+    "Implementador",
+    "Analista de Mesa de Servicio",
+    "Consultor Técnico",
+    "Implementador",
+    "Lider Técnico Arquitecto de Soluciones"
+]
+Fecha Inicio: Fecha en que inicia el proyecto
+Fecha Finalización: Fecha en que finaliza el proyecto
+
+Referencias
+Recomendaciones de compañeros, familiares, exjefes o personas a cargo.
+Fecha Modificación Fecha en que se modifica el registo.
+Nombre quien refiere: Nombre de la persona que recomienda o refiere.
+Mail quien refiere: Mail de la persona que recomienda o refiere.
+ Cargo quien refiere: Cargo de la persona que recomienda o refiere.
+ Cargo al que aspira: Cargo al que se postula
+ Fecha de referencia: Fecha en que se realiza la recomendación
+'''
+
+# Seteo de Ruta completa
+directorio_actual = os.getcwd()
+dir_F = f'{directorio_actual}/CV'
+carpeta = dir_F
+
+# Configuración inicial
+load_dotenv(".env")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# ----------Funciones de lectura de documentos----
+
+def consult_openai_personalizada(text, plantilla):
+    promp_PP = 'Del siguiente CV, identifica y  formatea la salida a la plantilla que te voy a compartir,en cuanto no encuentres algún elemento , coloca el sig texto "N/A":'
+    promp_p = (
+        f"{promp_PP} : \n--Inicio CV-- \n{text} \n--Fin CV-- \n \n--Inicio Plantilla--\n {plantilla} \n --Fin Plantilla--\n")
+    response_personalizada = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user",
+             "content": f"{promp_p}"}
+        ],
+        max_tokens=1000,
+        temperature=0.7,
+    )
+
+    return response_personalizada.choices[0].message.content
+
+def leer_docx(archivo):
+    doc = docx.Document(archivo)
+    texto_completo = [para.text for para in doc.paragraphs]
+    return '\n'.join(texto_completo)
+
+
+def leer_pdf_con_fitz(archivo):
+    doc = fitz.open(stream=archivo.read(), filetype="pdf")
+    texto_completo = ""
+    for pagina in doc:
+        texto_completo += pagina.get_text()
+    return texto_completo
+
+##Funcion para leer Fichero
+def leer_pdf(archivo):
+    texto = ""
+    with fitz.open(archivo) as doc:
+        for pagina in doc:
+            texto += pagina.get_text()
+    return texto
+
+
+
+st.title('Módulo de Carga de Archivos')
+st.markdown(
+    'Se muestran los archivos listos para procesar:')
+st.info(
+    f"!!IMPORTANTE!!: Para poder analzar los CV, deberas cargar los archivos dentro de la sig ruta:\n{dir_F}",
+    icon="👾",
+)
+
+if len(os.listdir(carpeta)) <= 0:
+    st.error(
+        "Por el momento no contamos con archivos pendientes de procesar. Revise si la carpeta se encuentra vacía",
+        icon="😞"
+    )
+
+for idx,archivo in enumerate(os.listdir(carpeta)):
+    st.write(f"Archivo {idx+1}: {archivo}")
+
+if st.button('Procesar Archivos'):
+    with st.spinner("Cargando CVs... Esto puede tomar unos minutos. Por favor espere"):
+        contenido_total = ""
+        # Esto podría ser mejor manejado como un diccionario si quieres mantener un mapeo directo
+        # entre el nombre del archivo y su contenido.
+        nombres_archivos = []
+        for archivo in os.listdir(carpeta):
+            if archivo.endswith('.pdf') or archivo.endswith('.docx'):
+                nombres_archivos.append(archivo)  # Guarda el nombre del archivo para uso posterior
+                ruta_completa = os.path.join(carpeta, archivo)
+                # Aquí agregas un separador único o algo que te permita dividir fácilmente luego
+                contenido_total += f"----- Separador de CV: {archivo} -----\n"
+                if archivo.endswith('.pdf'):
+                    contenido_total += leer_pdf(ruta_completa) + "\n"
+                else:  # archivo.endswith('.docx')
+                    contenido_total += leer_docx(ruta_completa) + "\n"
+        if contenido_total:
+            # Suponiendo que consult_openai_personalizada ahora devuelve algo que puedes dividir
+            # en secciones individuales por CV.
+            response_personalizada = contenido_total
+            # Asumiendo que response_personalizada es un texto donde cada CV está separado por un marcador único.
+            cv_secciones = response_personalizada.split(
+                "----- Separador de CV")  # Ajusta según sea necesario
+
+            # --CREACION DEL INDICE--#
+            app_pinecone.create_index()
+            list_text_resume = []
+            for seccion in cv_secciones[1:]:  # [1:] para saltar el primer elemento si está vacío
+                nombre_cv, contenido_cv = seccion.split("-----", 1)
+                # Divide en nombre y contenido
+                text_AF = contenido_cv.strip()
+                formateo = consult_openai_personalizada(text_AF, informacion_recopilar)
+                list_text_resume.append(formateo)
+
+            app_pinecone.insert_records(list_text_resume)
+
+        #st.session_state['records_inserted'] = True
+    st.success("Los registros se han insertado con éxito.")
+
+#if st.session_state.get('records_inserted', False):
+        # Código que se ejecuta si los registros se insertaron correctamente
+        #st.success("Los registros se han insertado con éxito.")
